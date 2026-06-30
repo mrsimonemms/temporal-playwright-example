833ecc3412d86695e046ab83d3f4f3b4c51a0c28
Simon Emms <simon@simonemms.com>
2026-06-30 12:21:10 +0000

    feat: build a simple login page
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- src/server.ts
+++ src/server.ts
@@ -13,20 +13,122 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import formbody from '@fastify/formbody';
+import fastifySecureSession from '@fastify/secure-session';
+import fastifyView from '@fastify/view';
 import fastify from 'fastify';
+import pug from 'pug';
+
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface ISession {
+  error?: string;
+  user?: IUser;
+}
+
+const users: IUser[] = [
+  {
+    id: 1,
+    name: 'User 1',
+    email: 'user1@temporal.io',
+    password: 'q1w2e3r4',
+  },
+  {
+    id: 2,
+    name: 'User 2',
+    email: 'user2@temporal.io',
+    password: 'q1w2e3r4',
+  },
+];
 
 async function main() {
   const server = fastify({
     logger: true,
   });
 
-  server.get('/ping', () => {
-    return 'pong';
-  });
+  // Order is important
+  server
+    .register(formbody)
+    .register(fastifySecureSession, {
+      // Hardcoded secret is fine for a demo, but not production
+      secret: 'this is a super secret secure session key',
+      salt: 'mq9hDxBVDbspDR6n',
+      cookie: {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: false,
+      },
+    })
+    .register(fastifyView, {
+      engine: {
+        pug,
+      },
+    })
+    .decorateRequest('user')
+    .addHook('preHandler', (req, _, done) => {
+      req.user = req.session.get('user');
+      done();
+    });
+
+  server
+    .get('/', (req, res) => {
+      if (!req.user) {
+        return res.redirect('/login');
+      }
+
+      return res.viewAsync('src/views/pages/index', {
+        title: 'Homepage',
+        user: req.user,
+      });
+    })
+    .get('/login', (req, res) => {
+      if (req.user) {
+        return res.redirect('/');
+      }
+
+      const err = req.session.get('error');
+
+      // Delete the error - treat as a flash message
+      req.session.set('error', undefined);
+
+      return res.viewAsync('src/views/pages/login', {
+        title: 'Login',
+        user: req.user,
+        err,
+      });
+    })
+    .post<{ Body: Omit<IUser, 'id' | 'name'> }>('/login', (req, res) => {
+      // Yes, this is a dreadful way to check password. But this is a demo
+      const user = users.find(
+        (item) =>
+          item.email === req.body.email && item.password === req.body.password,
+      );
+
+      if (user) {
+        req.log.info({ user: user.email }, 'User logged in');
+        req.session.user = user;
+      } else {
+        req.log.debug('Unknown user');
+        req.session.error = 'Unknown email/password';
+      }
+
+      return res.redirect('/');
+    })
+    .get('/logout', async (req, res) => {
+      req.session.regenerate();
+
+      return res.redirect('/');
+    });
 
   await server.listen({
     host: process.env.HOST ?? '0.0.0.0',
-    port: Number(process.env.POST ?? 3000),
+    port: Number(process.env.PORT ?? 3000),
   });
 }
 
